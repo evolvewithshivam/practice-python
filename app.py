28215dba5882357ea4eb123dec46166e350a8f3b
evolvewithshivam <evolvewithshivam@gmail.com>
2024-11-07 13:21:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,9 @@ import streamlit as st
 st.title('Welcome to Your Data Science First Streamlit App')
 st.write('Shivam')
 
-st.write("Most objects") # df, err, func, keras!
-st.write(["st", "is <", 3])
-st.write_stream(my_generator)
-st.write_stream(my_llm_stream)
-
-st.text("Fixed width text")
-st.markdown("_Markdown_")
-st.latex(r""" e^{i\pi} + 1 = 0 """)
-st.title("My title")
-st.header("My header")
-st.subheader("My sub")
-st.code("for i in range(8): foo()")
-st.html("<p>Hi!</p>")
+st.button("Click me")
+st.download_button("Download file", data)
+st.link_button("Go to gallery", url)
+st.page_link("app.py", label="Home")
+st.data_editor("Edit data", data)
+st.checkbox("I agree")
