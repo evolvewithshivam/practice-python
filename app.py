d05b9cdbb789f80766fc90757999faea7dd30e46
evolvewithshivam <evolvewithshivam@gmail.com>
2024-11-07 13:20:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,17 @@ import streamlit as st
 # Add a simple title
 st.title('Welcome to Your Data Science First Streamlit App')
 st.write('Shivam')
+
+st.write("Most objects") # df, err, func, keras!
+st.write(["st", "is <", 3])
+st.write_stream(my_generator)
+st.write_stream(my_llm_stream)
+
+st.text("Fixed width text")
+st.markdown("_Markdown_")
+st.latex(r""" e^{i\pi} + 1 = 0 """)
+st.title("My title")
+st.header("My header")
+st.subheader("My sub")
+st.code("for i in range(8): foo()")
+st.html("<p>Hi!</p>")
